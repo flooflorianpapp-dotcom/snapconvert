58fdb388af708d68ec8c2c6dacf4b3bab4426145
flooflorianpapp-dotcom <flooflorianpapp@gmail.com>
2026-03-15 13:10:17 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,12 +42,7 @@ export default function RootLayout({
   return (
    <html lang="en">
   <head>
-    <Script
-      async
-      src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7074188379523382"
-      crossOrigin="anonymous"
-      strategy="afterInteractive"
-    />
+    <meta name="google-adsense-account" content="ca-pub-7074188379523382">
   </head>
 
   <body className="font-sans antialiased">
